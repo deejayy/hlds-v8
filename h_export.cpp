9cb726def779b07e41b6e8336d95ce352da1de68
nb <deejayy@deejayy.hu>
2014-03-08 16:52:55 +0100

    strip header comments

--- h_export.cpp
+++ h_export.cpp
@@ -1,30 +1,3 @@
-// vi: set ts=4 sw=4 :
-// vim: set tw=75 :
-
-// From SDK dlls/h_export.cpp:
-
-/***
-*
-*	Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
-*
-*   Use, distribution, and modification of this source code and/or resulting
-*   object code is restricted to non-commercial enhancements to products from
-*   Valve LLC.  All other use, distribution, or modification is prohibited
-*   without written permission from Valve LLC.
-*
-****/
-/*
-
-===== h_export.cpp ========================================================
-
-  Entity classes exported by Halflife.
-
-*/
-
 #include <extdll.h>
 
 #include <h_export.h>
